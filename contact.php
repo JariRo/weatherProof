144f3b354a384dd8af9f582b52784e431610645e
JariRo <jarirosenstrom@gmail.com>
2017-09-20 21:12:02 +1000

    current live version

--- contact.php
+++ contact.php
@@ -1,27 +1,27 @@
 <?php
     if(isset($_POST['singlebutton'])){
-        $name = $_POST['thename'];                             //variablar från html-dokumentet
-        $epost = $_POST['theEmail'];
-        $message = $_POST['themessage'];
+        $name = $_POST['namn'];                             //variablar från html-dokumentet
+        $epost = $_POST['email'];
+        $message = $_POST['contact-message'];
         $callme = $_POST['callme'];
-        $phoneNr = $_POST['phonenumber'];
+        $phoneNr = $_POST['phoneNumber'];
 
         require 'PHPMailer/PHPMailerAutoload.php';
         $mail = new PHPMailer;
 
-        $mail->SMTPDebug = 3;                               // Enable verbose debug output
+        //$mail->SMTPDebug = 3;                               // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'mailcluster.loopia.se';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'username';                 // SMTP username
-        $mail->Password = 'password';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;
+        $mail->Host = 'localhost';  // Specify main and backup SMTP servers
+        //$mail->SMTPAuth = true;                               // Enable SMTP authentication
+        //$mail->Username = 'username';                 // SMTP username
+        //$mail->Password = 'password';                           // SMTP password
+        //$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 25;
         $mail->CharSet = 'UTF-8';                           // TCP port to connect to
 
 
         $mail->setFrom($epost, "$name");
-        $mail->addAddress('Info@weatherproof.se', 'Kontakt');     // Add a recipient
+        $mail->addAddress('jarirosenstrom@gmail.com', 'Kontakt');     // Add a recipient
         $mail->isHTML(true);                                  // Set email format to HTML
 
         $mail->Subject = 'Kontaktformulär';
